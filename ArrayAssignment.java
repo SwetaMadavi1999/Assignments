eb2003594bb98995fe03945ea3c00899537be458
SwetaMadavi1999 <95091282+SwetaMadavi1999@users.noreply.github.com>
2021-12-31 12:01:10 +0530

    Create ArrayAssignment.java

---
+++ ArrayAssignment.java
@@ -0,0 +1,66 @@
+/* 1. java program to creating an array 
+solution*/
+public class Main1{
+public static void main(String[] args){
+int[] arr=new int[3];
+arr[0]=10;
+arr[1]=20;
+arr[2]=30;
+for(int i=1;i<arr.length;i++){
+System.out.println("element of index"+i+a[i]);
+}
+}}
+
+
+/*2.java program for narrowing casting
+solution*/
+public class Main2{
+public static void main(String args[]){
+double myDouble=9.78;
+int myInt=(int) myDouble;
+System.out.println(myDouble);
+System.out.println(myInt);
+}}
+
+
+/*3.display the data of student
+solution*/
+public class Main3{
+public static void main(String args[]){
+int rollno;
+String name;
+double marks;
+rollno=21;
+name="Sweta";
+marks=86.5;
+System.out.println("your name is:"+name);
+System.out.println("your roll number is:"+rollno);
+System.oit.println("your marks is:"+marks);
+}}
+
+
+/*4. swap two numbers
+solution*/
+public class Main4{
+public static void main(String args[]){
+int a=60;
+int b=70;
+int temp;
+System.out.println("before swapping a:"+a+" and b:"+b);
+temp=a;
+a=b;
+b=temp;
+System.out.println("after swapping a:"+a+" and b"+b);
+}}
+
+
+/*5.java program for widening casting
+solution*/
+public class Main5{
+public static void main(String args[]){
+int myInt=15;
+double myDouble=myInt;
+System.out.println(myInt);
+System.out.println(myDouble);
+}
+}
