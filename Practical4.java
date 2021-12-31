dd910613e8813c08576e11e16fee54d5031218fd
SwetaMadavi1999 <95091282+SwetaMadavi1999@users.noreply.github.com>
2021-12-31 12:39:03 +0530

    Create Practical4.java

---
+++ Practical4.java
@@ -0,0 +1,54 @@
+// For Array List
+import java.util.*;
+public class ArrayList{
+  public static void main(String[] args){
+    List<String> list_Strings = new ArrayList<String>();
+    list_Strings.add("Lily");
+    list_Strings.add("Lotus");
+    list_Strings.add("Rose");
+    list_Strings.add("Sunflower");
+    list_Strings.add("Daisy");
+    System.out.println(list_Strings);
+  }}
+
+// For Hash Set
+import java.util.Hashset;
+public class Hashset1{
+  public static void main(String[] args){
+    Hashset<String> h_set = new Hashset<String>();
+    h_set.add("Black");
+    h_set.add("Purple");
+    h_set.add("Pink");
+    h_set.add("White");
+    h_set.add("Red");
+    System.out.println("The Hash Set: "+h_set);
+  }}
+
+//For Linked List
+import java.util.LinkedList;
+public class LinkedList1 {
+  public static void main(String[] args){
+    LinkedList<String> l_list = new LinkedList<String>();
+    l_list.add("Jimin");
+    l_list.add("Jim");
+    l_list.add("Jhope");
+   l_list.add("Namjoon");
+   l_list.add("Suga");
+    System.out.println("The Linked list: "+l_list);
+  }}
+// For Tree Set
+
+import java.util.TreeSet;
+public class TreeSet1 {
+  public static void main(String[] args){
+    TreeSet<String> tree_set = new Treeset<String>();
+    tree_set.add("Orange");
+    tree_set.add("Mango");
+    tree_set.add("Banana");
+    tree_set.add("Papaya");
+    tree_set.add("Grapes");
+    System.out.println("Tree Set: ");
+    System.out.println(tree_set);
+  }}
+
+
