add4fb3afaff63d8baa53bbbeb6cfe279e264a88
SwetaMadavi1999 <95091282+SwetaMadavi1999@users.noreply.github.com>
2022-01-17 13:37:26 +0530

    Create StackProgram.java

---
+++ StackProgram.java
@@ -0,0 +1,21 @@
+package testjava;
+import java.util.Collections;
+
+import java.util.List;
+import java.util.Stack;
+public class StackProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Stack<String> list = new Stack<String>();
+list.push("Num1");
+list.push("Num2");
+list.push("Num3");
+list.push("Num4");
+System.out.println("The string is"+list);
+	}
+
+}
+/* Output:
+The string is[Num1, Num2, Num3, Num4]
+*/
