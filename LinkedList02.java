c40f530185154f7be503ae751210e149d0cce905
SwetaMadavi1999 <95091282+SwetaMadavi1999@users.noreply.github.com>
2022-01-17 13:34:40 +0530

    Create LinkedList02.java

---
+++ LinkedList02.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class LinkedList01{
+public static void main(String[] args){
+LinkedList<String> l_list = new LinkedList<String>();
+l_list.add("Pink");
+l_list.add("Blue");
+l_list.add("Yellow");
+l_list.add("Purple");
+l_list.add("Brown");
+System.out.println("The linked list: "+l_list);
+
+Iterator<String>itrIterable=list.iterator();
+while (itrIterable.hasnext());{
+System.out.println(itrIterable.next());
+}}}
