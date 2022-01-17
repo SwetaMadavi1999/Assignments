f0a1df8baa7f984f2d6fec59cc356df88c712b52
SwetaMadavi1999 <95091282+SwetaMadavi1999@users.noreply.github.com>
2022-01-17 13:27:49 +0530

    Create HashSet01.java

---
+++ HashSet01.java
@@ -0,0 +1,12 @@
+
+	import java.util.HashSet;
+public class Hashset01 {
+public static void main(String[] args) {
+Hashset<String> h_set = new HashSet<String>();
+h_set.add("Banana");
+h_set.add("Strawberry");
+h_set.add("Grapes");
+h_set.add("Pomegranate");
+h_set.add("Watermelon");
+System.out.println("The Hash Set: " +h_set);
+}}
