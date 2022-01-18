8d03326dad67998a912729f7a3013b0ec28f54b5
SwetaMadavi1999 <95091282+SwetaMadavi1999@users.noreply.github.com>
2022-01-18 12:55:37 +0530

    Create SwapProgram.java

---
+++ SwapProgram.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int temp;
+System.out.println("before swapping a:"+a+" and b:"+b);
+temp=a;
+a=b;
+b=temp;
+System.out.println("after swapping a:"+a+ " and b :"+b);
+	}
+}
+/* Output:
+before swapping a:10 and b:20
+after swapping a:20 and b:10
+*/
